99003f80ec60a1e4e0835785b65f43c1c3f94f88
Kazhumurat <72253973+Kazhumurat@users.noreply.github.com>
2020-11-22 18:28:41 +0300

    Update power.c

--- power.c
+++ power.c
@@ -1,22 +1,21 @@
 #include<stdio.h> 
-  
+
 /* Function to calculate x raised to the power y */
-int power(int a,  unsigned int n) 
-{ 
-    if (n == 0) 
-        return 1; 
-    else if (n%2 == 0) 
-        return power(a, n/2)*power(a, n/2); 
-    else
-        return a*power(a, n/2)*power(a, n/2); 
+int power(int a, int n) 
+{
+	// write a function to calculate power
+  if (n != 0)
+      return (a * power(a, n - 1));
+  else
+      return 1;
 } 
-  
+
 /* Program to test function power */
 int main() 
 { 
-    int a = 2; 
-    unsigned int n = 3; 
-  
-    printf("%d", power(a, n)); 
-    return 0; 
-}
+	int a = 2; 
+	int n = 3; 
+
+	printf("%d", power(a, n)); 
+	return 0; 
+} 
